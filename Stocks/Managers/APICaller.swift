bc56f355a16ad54483999053ad142a53aa879403
Vitor Otero <vitorotero@icloud.com>
2022-04-09 20:30:34 +0100

    Add manager files

---
+++ Stocks/Managers/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  Stocks
+//
+//  Created by Vitor Otero on 09/04/2022.
+//
+
+import Foundation
---
+++ Stocks/Managers/PersistenceManager.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceManager.swift
+//  Stocks
+//
+//  Created by Vitor Otero on 09/04/2022.
+//
+
+import Foundation
